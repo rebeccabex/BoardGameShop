6d876f0f66e3a298248af4155df478d695a2e6b2
rebeccabex <rebeccabexb@gmail.com>
2018-01-29 17:27:35 +0000

    First exercises

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,32 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    implicit request: Request[AnyContent] =>
+      Ok(views.html.index("Hello world!"))
+  }
+
+  def indexRedirect = Action {
+    implicit request: Request[AnyContent] =>
+      Redirect("/index")
+  }
+
+  def plannedUpdates = Action {
+    implicit request: Request[AnyContent] =>
+      Ok("Planned technical updates. Please come back later.")
+  }
+
+  def page = Action {
+    implicit request: Request[AnyContent] =>
+      Ok(<ul><li>Hello world!</li><li>New page</li></ul>).as(HTML)
+  }
+
+  def displayGame(game: String) = Action {
+    implicit request: Request[AnyContent] =>
+      Ok(s"$game is not currently in stock. Please come back later.")
   }
 
 }
