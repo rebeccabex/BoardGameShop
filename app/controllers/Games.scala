40983e4fb80b9d75a19c2ef7b8aa202ab7f72ab3
rebeccabex <rebeccabexb@gmail.com>
2018-01-30 15:39:35 +0000

    Product pages for each category

--- app/controllers/Games.scala
+++ app/controllers/Games.scala
@@ -5,18 +5,22 @@ import play.api.mvc.{Action, AnyContent, Controller, Request}
 class Games extends Controller {
 
   val gamesList = Array(
-    ("Hive", "Insect tile game", "hive.jpg", 20.0, Array("Two Player", "Strategy")),
-    ("Scythe", "Area control and resource management game", "scythe.jpg", 70.0, Array("One Player", "Strategy")),
-    ("Love Letter", "Fast playing card game", "loveLetter.jpg", 12.0, Array("Family", "Quick")),
-    ("Celestia", "Airship adventure game", "celestia.jpg", 28.0, Array("Family", "Strategy")),
-    ("Agricola", "17th Farming game", "agricola.jpg", 60.0, Array("One Player", "Strategy"))
+    ("Hive", "Insect tile game", "hive.jpg", 20.0, Array("twoPlayer", "strategy")),
+    ("Scythe", "Area control and resource management game", "scythe.jpg", 70.0, Array("onePlayer", "strategy")),
+    ("Love Letter", "Fast playing card game", "loveLetter.jpg", 12.0, Array("family", "quick")),
+    ("Celestia", "Airship adventure game", "celestia.jpg", 28.0, Array("family", "strategy")),
+    ("Agricola", "17th Farming game", "agricola.jpg", 60.0, Array("onePlayer", "strategy"))
   )
 
   def games = Action {
     implicit request: Request[AnyContent] =>
-      Ok(views.html.games(gamesList))
+      Ok(views.html.games(gamesList, "all"))
   }
 
+  def gamesByCategory(category: String) = Action {
+    implicit request: Request[AnyContent] =>
+      Ok(views.html.games(gamesList, category))
+  }
 
   def displayGame(game: String) = Action {
     implicit request: Request[AnyContent] =>
