880a638f91365c63e148cb8fed30da9bb2eff667
rebeccabex <rebeccabexb@gmail.com>
2018-01-30 17:00:49 +0000

    Fixed multi-word name issue for item pages

--- app/controllers/Games.scala
+++ app/controllers/Games.scala
@@ -5,11 +5,11 @@ import play.api.mvc.{Action, AnyContent, Controller, Request}
 class Games extends Controller {
 
   val gamesList = Array(
-    ("Hive", "Insect tile game", "hive.jpg", 20.0, Array("twoPlayer", "strategy")),
-    ("Scythe", "Area control and resource management game", "scythe.jpg", 70.0, Array("onePlayer", "strategy")),
-    ("Love Letter", "Fast playing card game", "loveLetter.jpg", 12.0, Array("family", "quick")),
-    ("Celestia", "Airship adventure game", "celestia.jpg", 28.0, Array("family", "strategy")),
-    ("Agricola", "17th Farming game", "agricola.jpg", 60.0, Array("onePlayer", "strategy"))
+    ("hive", "Hive", "Insect tile game", "hive.jpg", 20.0, Array("twoPlayer", "strategy")),
+    ("scythe", "Scythe", "Area control and resource management game", "scythe.jpg", 70.0, Array("onePlayer", "strategy")),
+    ("love-letter", "Love Letter", "Fast playing card game", "loveLetter.jpg", 12.0, Array("family", "quick")),
+    ("celestia", "Celestia", "Airship adventure game", "celestia.jpg", 28.0, Array("family", "strategy")),
+    ("agricola", "Agricola", "17th Farming game", "agricola.jpg", 60.0, Array("onePlayer", "strategy"))
   )
 
   def games = Action {
@@ -30,7 +30,7 @@ class Games extends Controller {
   def findGameByName(gameName: String) =
     gamesList.find(_._1 == gameName) match {
       case Some(game) => game
-      case None => ("missing", "no description", "blank.jpg", 0.0, Array(""))
+      case None => ("missing", "missing", "no description", "blank.jpg", 0.0, Array(""))
     }
 
 }
