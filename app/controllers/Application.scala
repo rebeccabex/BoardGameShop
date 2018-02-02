ad784556fb50c134341d5575135c4c2ef4a4b031
rebeccabex <rebeccabexb@gmail.com>
2018-02-02 15:49:16 +0000

    Added featured games to home page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,16 @@
 package controllers
 
-import models.ShoppingBasket
+import models.{GameItem, ShoppingBasket}
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index("Welcome!", ShoppingBasket.shoppingBasket.toList))
+    Ok(views.html.index("Welcome!", ShoppingBasket.shoppingBasket.toList, GameItem.gamesList))
   }
 
   def indexRedirect = Action { implicit request: Request[AnyContent] =>
-    Redirect("/index")
+    Redirect(routes.Application.index())
   }
 
   def page = Action { implicit request: Request[AnyContent] =>
@@ -18,7 +18,7 @@ class Application extends Controller {
   }
 
   def plannedUpdates = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index("Planned technical updates. Please come back later.", ShoppingBasket.shoppingBasket.toList))
+    Ok(views.html.index("Planned technical updates. Please come back later.", ShoppingBasket.shoppingBasket.toList, GameItem.gamesList))
   }
 
 }
